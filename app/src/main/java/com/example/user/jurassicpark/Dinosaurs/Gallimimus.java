85ad43368f2f78ef35135edadc067ebf6aea32a3
KatyPreston <prestonkj@hotmail.com>
2018-06-01 14:44:09 +0100

    all dinosaur classes created and getters tested

---
+++ app/src/main/java/com/example/user/jurassicpark/Dinosaurs/Gallimimus.java
@@ -0,0 +1,8 @@
+package com.example.user.jurassicpark.Dinosaurs;
+
+public class Gallimimus extends Dinosaur {
+
+    public Gallimimus(FeedType feedType, String type, String name){
+        super(feedType, type, name);
+    }
+}
---
+++ app/src/test/java/com/example/user/jurassicpark/GallimimusTest.java
@@ -0,0 +1,39 @@
+package com.example.user.jurassicpark;
+
+import com.example.user.jurassicpark.Dinosaurs.FeedType;
+import com.example.user.jurassicpark.Dinosaurs.Gallimimus;
+import com.example.user.jurassicpark.Dinosaurs.Pteranodon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GallimimusTest {
+    Gallimimus gallimimus;
+
+    @Before
+    public void before(){
+        gallimimus = new Gallimimus(FeedType.OMNIVORE, "Gallimimus", "Fergus");
+    }
+
+    @Test
+    public void canGetFeedType(){
+        assertEquals(FeedType.OMNIVORE, gallimimus.getFeedType());
+    }
+
+    @Test
+    public void canGeType(){
+        assertEquals("Gallimimus", gallimimus.getType());
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Fergus", gallimimus.getName());
+    }
+
+    @Test
+    public void canEat(){
+        assertEquals("Mmmm", gallimimus.eat());
+    }
+}
