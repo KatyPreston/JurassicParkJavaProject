9bf9f75810e69835af6fcb1d35e0a162f7395719
KatyPreston <prestonkj@hotmail.com>
2018-06-01 14:40:56 +0100

    velociraptor class added

---
+++ app/src/main/java/com/example/user/jurassicpark/Dinosaurs/Velociraptor.java
@@ -0,0 +1,8 @@
+package com.example.user.jurassicpark.Dinosaurs;
+
+public class Velociraptor extends Dinosaur{
+
+    public Velociraptor(FeedType feedType, String type, String name){
+        super(feedType, type, name);
+    }
+}
---
+++ app/src/test/java/com/example/user/jurassicpark/VelociraptorTest.java
@@ -0,0 +1,41 @@
+package com.example.user.jurassicpark;
+
+import android.view.ViewGroup;
+
+import com.example.user.jurassicpark.Dinosaurs.FeedType;
+import com.example.user.jurassicpark.Dinosaurs.Pteranodon;
+import com.example.user.jurassicpark.Dinosaurs.Velociraptor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VelociraptorTest {
+    Velociraptor velociraptor;
+
+    @Before
+    public void before(){
+        velociraptor = new Velociraptor(FeedType.CARNIVORE, "Velociraptor", "Blue");
+    }
+
+    @Test
+    public void canGetFeedType(){
+        assertEquals(FeedType.CARNIVORE, velociraptor.getFeedType());
+    }
+
+    @Test
+    public void canGeType(){
+        assertEquals("Velociraptor", velociraptor.getType());
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Blue", velociraptor.getName());
+    }
+
+    @Test
+    public void canEat(){
+        assertEquals("Mmmm", velociraptor.eat());
+    }
+}
