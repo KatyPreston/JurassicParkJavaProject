164d988901ade0ca947cac05b98279992a6abc2a
KatyPreston <prestonkj@hotmail.com>
2018-06-01 13:20:58 +0100

    aviary class created

--- app/src/main/java/com/example/user/jurassicpark/Paddocks/Aviary.java
+++ app/src/main/java/com/example/user/jurassicpark/Paddocks/Aviary.java
@@ -1,4 +1,12 @@
 package com.example.user.jurassicpark.Paddocks;
 
+import com.example.user.jurassicpark.Dinosaurs.Dinosaur;
+
+import java.util.ArrayList;
+
 public class Aviary extends Paddock {
+
+    public Aviary(PaddockType type, ArrayList<Dinosaur> dinosaurList){
+        super(type, dinosaurList);
+    }
 }
