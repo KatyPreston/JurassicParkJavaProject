fca1aba6b138816e2c73417228eadfc50e553f00
KatyPreston <prestonkj@hotmail.com>
2018-06-04 11:01:38 +0100

    visitor class created and getters wrking

---
+++ app/src/test/java/com/example/user/jurassicpark/VisitorTest.java
@@ -0,0 +1,27 @@
+package com.example.user.jurassicpark;
+
+import com.example.user.jurassicpark.Visitors.Visitor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisitorTest {
+    Visitor visitor;
+
+    @Before
+    public void before(){
+        visitor = new Visitor("Janet", 100);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Janet", visitor.getName());
+    }
+
+    @Test
+    public void canGetWallet(){
+        assertEquals(100, visitor.getWallet(), 0.1);
+    }
+}
